8b2ccecb9d94700dd1f668069b623e22407f6e10
Einar Strandberg <einargs@gmail.com>
2019-03-04 18:52:51 -0600

    Updated the README and added comments to `run-pass.sh`.

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
+# Runtime LTL
+This is an undergraduate research project aiming towards creating a runtime
+verification process for C++ code that will check the function call graph
+against an LTL formula.
 
+## Process
+This is done by creating an LLVM IR pass that inserts calls to a verification
+system at the beginning and end of annotated functions.
+
+Functions are annotated like so:
+```c
+__attribute__((annotate("ltl_verify"))) void annotate_this() {
+  printf("Hello world!\n");
+}
+```
+
+## Requirements
+- CMake >= 3.11
+- ninja 1.8.2
+- LLVM 7 (`llvm-config` and `lli` should be in your `PATH`)
+- Clang 7
+
+## Building
+To build, please run `configure`, which will setup a `build` directory to use
+CMake with Ninja as the target build system. If you are familiar with CMake,
+feel free to set this up to use other build systems. The configure script will
+perform an initial build of the shared object; afterwards, to re-build upon
+changes to the source, run `cmake --build $PROJECT_ROOT/build`.
+
+## Using Nix
+If you use [Nix](https://nixos.org/nix/) you can simply run `shell.nix` to
+install the required dependencies. Further, you can build the shared object
+with `nix-build`. Make sure that the `build` directory does not exist, as it
+will be copied by `nix-build` and conflict with the new `build` directory the
+nix build process will try to generate.
+
+During development it is recommended to use the process described in
+[Building](#building) to avoid rebuilding the shared object from scratch after
+every change.
+
+## Using `run-pass.sh`
+`run-pass.sh` is a script for quickly trying out the runtime ltl plugin on
+an example `c` file. It is designed to deal with both `nix` and non-`nix`
+environments. The script is commented, but rough, as I am not very familiar
+with bash scripting.
+
+The script will print to STDOUT the generated IR and then execute it using the
+`lli` interpreter.
+
+Syntax:
+```
+[PASS_SO=<plugin shared object>] ./run-pass.sh <source input> [<IR output file>]
+```
+
+If `PASS_SO` is an environment variable, it will use that as the shared object
+for dynamically linking the plugin. The first argument is the source file,
+and the second argument is an optional file to output the generated IR to.
--- run-pass.sh
+++ run-pass.sh
@@ -1,34 +1,71 @@
 #!/usr/bin/env bash
 
+SO_NAME="RuntimeLtlProject.so"
 TARGET_FILE=$1
 
+# If no argument is passed for the output, create a temporary file instead.
 if [ "$2" != "" ]; then
+  echo "Outputing IR to $2"
   OUTPUT_FILE=$2
 else
   OUTPUT_FILE=$(mktemp)
+  # delete the temp file when the script exits.
   trap 'rm -f $OUTPUT_FILE' EXIT
 fi
 
-echo "OUTPUT_FILE is $OUTPUT_FILE"
+# If `PASS_SO` is an environment variable, don't override it.
+# This allows you to specify a different location for the script to find
+# the plugin at.
+if [ -z PASS_SO ]; then 
+  # location of the shared object in the local result of a `nix-build`
+  local RESULT_SO="./result/$SO_NAME"
+  # location of the shared object as a result of a normal CMake build
+  local BUILD_SO="./build/RuntimeLtlProject/$SO_NAME"
 
-if [ -f ./result/RuntimeLtlProject.so ]; then
-  PASS_SO=./result/RuntimeLtlProject.so
-else
-  PASS_SO="./build/RuntimeLtlProject/RuntimeLtlProject.so"
+  if [ -f $RESULT_SO ]; then
+    # If there is a result folder with a shared object in it, use that;
+    PASS_SO=$RESULT_SO
+  elif [ -f $BUILD_SO ]; then
+    # Otherwise, check if a shared object exists as a result of a normal
+    # CMake build.
+    PASS_SO=$BUILD_SO
+  else
+    # If a shared object cannot be located, exit with an error.
+    echo "Error: Could not find shared object."
+    exit 1
+  fi
 fi
 
-
+# If `nix-shell` has been run with the local `shell.nix`, use the `$clang7`
+# variable to execute the code. Otherwise, locate the `clang` command in the
+# user's environment.
+#
+# The reason this work around is neccessary is because currently in nix clang
+# when build with libraries will break an environmental variable that GCC
+# depends on.
 if [ $USING_NIX_SHELL == 1 ]; then
   CLANG="$clang7"
 else
   CLANG=$(/usr/bin/env clang)
 fi
 
+# Make sure that a clang executable has been found.
+if [ -z $CLANG ]; then
+  echo "Error: could not find clang executable."
+  exit 1
+fi
+
+# Echo the path of the `clang` executable being used.
 echo "CLANG is $CLANG"
 
+# Load the shared object containing the pass into clang and emit the LLVM IR
+# into the output file.
 $CLANG -Xclang -load -Xclang $PASS_SO -O0 -emit-llvm $TARGET_FILE -S -o $OUTPUT_FILE
 
+# Print the IR to stdout.
 cat $OUTPUT_FILE
+
+# Then execute the IR inside the IR interpreter.
 echo "Running..."
 lli $OUTPUT_FILE
 
